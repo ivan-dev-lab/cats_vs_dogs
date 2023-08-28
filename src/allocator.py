ccb047a9a1ad8ce474d8a5a247cd938289fdfa01
ivan-dev-lab <roalivan4@gmail.com>
2023-08-28 21:30:16 +0500

    разрабатывается функция allocate

--- src/allocator.py
+++ src/allocator.py
@@ -8,10 +8,9 @@ def create_dirs (dir_name: str):
     path = os.path.join(ROOT_DIR, dir_name)
     if os.path.exists(path):
         shutil.rmtree(path)
-    else:
-        os.makedirs(path)
-        os.makedirs(os.path.join(path, "cat"))
-        os.makedirs(os.path.join(path, "dog"))
+    os.makedirs(path)
+    os.makedirs(os.path.join(path, "cat"))
+    os.makedirs(os.path.join(path, "dog"))
 
 
 def check_len (dir_name: str, dir_size: int) -> bool:
@@ -19,3 +18,33 @@ def check_len (dir_name: str, dir_size: int) -> bool:
         return True
     else: return False
 
+def allocate ():
+    [create_dirs(path) for path in ["train", "test", "control"]]
+
+    dataset_size = len(os.listdir(os.path.join(ROOT_DIR, "images")))
+    train_size, test_size, control_size = (0.7*dataset_size, 0.15*dataset_size, 0.15*dataset_size)
+
+    for img_name in os.listdir(os.path.join(ROOT_DIR, "images")):
+        src = os.path.join(ROOT_DIR, "images", img_name)
+        print("images",src.split("\\")[-1:][0] ,sep="/", end=" -> ")
+
+        pet = "cat" if img_name.find("cat") != -1 else "dog"
+
+        if check_len(os.path.join(ROOT_DIR, "train"), dir_size=train_size) == False:
+            if check_len(os.path.join(ROOT_DIR, "train", pet), dir_size=train_size/2) == False:
+                dst = os.path.join(ROOT_DIR, "train", pet, img_name)
+            else: continue
+        elif check_len(os.path.join(ROOT_DIR, "test"), dir_size=test_size) == False:
+            if check_len(os.path.join(ROOT_DIR, "test", pet), dir_size=test_size/2) == False:
+                dst = os.path.join(ROOT_DIR, "test", pet, img_name)
+            else: continue
+        elif check_len(os.path.join(ROOT_DIR, "control"), dir_size=control_size) == False:
+            if check_len(os.path.join(ROOT_DIR, "control", pet), dir_size=control_size/2) == False:
+                dst = os.path.join(ROOT_DIR, "control", pet, img_name)
+            else: continue
+        
+        print(dst.split("\\")[1], dst.split("\\")[2], dst.split("\\")[3], sep="/", end=" -> ")
+        print(len(os.listdir("/".join(dst.split("\\")[:-1]))))
+        shutil.copy(src, dst)   
+
+allocate()
